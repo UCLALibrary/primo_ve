81e2c3e279b54e7703bb1a4779d2658cfff6f663
Zoe Tucker <zoetucker@library.ucla.edu>
2022-08-09 12:41:00 -0700

    add view name parameter and refactor secret handling

--- primo-zip.py
+++ primo-zip.py
@@ -3,9 +3,10 @@ import shutil
 import fileinput
 import json
 import os
+import sys
 
 
-def swap_secrets_and_values(secret_data, types_to_check, directory, secret_visible=1):
+def swap_secrets_and_values(secret_data, types_to_check, directory, secret_visible=True):
 
     # var for file paths list
     file_paths = []
@@ -25,13 +26,14 @@ def swap_secrets_and_values(secret_data, types_to_check, directory, secret_visib
                     secret_value = str(
                         secret_data['secret_details'][cnt]['secret_value'])
 
-                    secret_swap = [secret_name, secret_value]
                     with fileinput.FileInput(filepath, inplace=True) as file:
                         for line in file:
-                            # Replaces secret_name with secret_value when secret_visible = 1;
-                            #  replaces secret_value with secret_name when secret_visible = 0
-                            print(line.replace(
-                                str(secret_swap[1-secret_visible]), secret_swap[secret_visible]), end='')
+                            # Replaces secret_name with secret_value when secret_visible is True
+                            # Replaces secret_value with secret_name when secret_visible is False
+                            if secret_visible:
+                                print(line.replace(secret_name, secret_value), end='')
+                            else:
+                                print(line.replace(secret_value, secret_name), end='')
                     cnt += 1
 
 
@@ -41,19 +43,16 @@ def zip_up_source(zip_name, base_directory, directory):
 
 
 def main():
+    # user must input zip_name parameter at command line
+    if len(sys.argv) > 1:
+        zip_name = sys.argv[1]
+    else:
+        sys.exit("Folder name parameter must be specified.")
+
     # zip info
     types_to_check = ('.js', '.css', 'html', 'md', 'txt')
     base_directory = '.'
-    directory = './01UCS_LAL-UCLA'
-    directory_test = './01UCS_LAL-Test_00'
-    zip_name = '01UCS_LAL-UCLA'
-    zip_name_test = '01UCS_LAL-Test_00'
-
-    # use the test folder if it is present
-    isdir = os.path.isdir(directory_test)
-    if(isdir):
-        directory = directory_test
-        zip_name = zip_name_test
+    directory = './' + zip_name
 
     # source of the secrets
     secrets = open('secrets.json')
@@ -62,14 +61,14 @@ def main():
 
     # find each file in dir structure, replace tags with secrets
     swap_secrets_and_values(secret_data, types_to_check,
-                            directory, secret_visible=1)
+                            directory, secret_visible=True)
 
     # zip the source for deploymeny to Primo
     zip_up_source(zip_name, base_directory, directory)
 
     # find each file in dir structure, replace secrets with tags
     swap_secrets_and_values(secret_data, types_to_check,
-                            directory, secret_visible=0)
+                            directory, secret_visible=False)
 
     print('Secrets inserted and files zipped: ' + zip_name + '.zip')
     print('Secrets cleared: code may now be pushed')
