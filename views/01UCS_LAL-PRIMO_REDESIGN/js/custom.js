e359ddb6927d33d26187f2a2cae851f8625a2471
Zoe Tucker <zoetucker@library.ucla.edu>
2023-11-16 11:58:16 -0800

    reformat text to left-aligned, centered block

--- views/01UCS_LAL-PRIMO_REDESIGN/js/custom.js
+++ views/01UCS_LAL-PRIMO_REDESIGN/js/custom.js
@@ -697,12 +697,12 @@ app.component("prmAlmaOtherMembersAfter", {
   });
   /* Ethical Description Note */
   app.component('ethicalDescriptionNote', {
-    template: `<div layout="row" class="alert-bar layout-align-center-center layout-row" layout-align="center center">
-      <span class="bar-text">We are committed to updating our catalog records and finding aids whenever feasible 
+    template: `<div id="ed-box">
+      <div id="ed-text">We are committed to updating our catalog records and finding aids whenever feasible 
       to revise problematic descriptions and subjects, including the addition of relevant context.
       <b>To report harmful language, please use 
       <a href="https://ucla.libwizard.com/id/38f45c482a5fcb0b715a7e9e3ddee8b2" target="_blank" rel="noopener noreferrer">this form</a>.</b> 
-      </span></div>`
+      </div></div>`
   });
 
   app.component('prmServiceDetailsAfter', {
